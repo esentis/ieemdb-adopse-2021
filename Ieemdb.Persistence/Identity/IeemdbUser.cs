3787ccf64f50f90cad24d3eec5bd71cf491a7444
George Leonidis <esentakos@yahoo.gr>
2021-03-13 22:56:22 +0200

    Creates IEEMdb Identity

---
+++ Ieemdb.Persistence/Identity/IeemdbUser.cs
@@ -0,0 +1,13 @@
+﻿using Kritikos.Configuration.Persistence.Abstractions;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Ieemdb.Persistence.Identity
+{
+    public class IeemdbUser : IdentityUser<Guid>, IEntity<Guid>, ITimestamped
+    {
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
