bfc8acb5a6bceab172c6bc0ee8975a30ed12a845
George Leonidis <esentakos@yahoo.gr>
2021-03-31 22:26:30 +0300

    Edits one to many models

--- src/Ieemdb.Persistence/Models/MovieGenre.cs
+++ src/Ieemdb.Persistence/Models/MovieGenre.cs
@@ -2,11 +2,9 @@ namespace Esentis.Ieemdb.Persistence.Models
 {
   using System;
 
-  using Esentis.Ieemdb.Persistence.Base;
-
   using Kritikos.Configuration.Persistence.Abstractions;
 
-  public class MovieGenre : Entity<long>, IAuditable<Guid>
+  public class MovieGenre : IAuditable<Guid>
   {
     public Movie Movie { get; set; }
 
