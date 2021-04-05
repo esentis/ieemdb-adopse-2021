addad94ad7549482a51e921fc393f14ce02a10cc
Δημήτρης Γαβριηλίδης <49337508+GavriilJim@users.noreply.github.com>
2021-04-05 16:10:37 +0300

    commit on feature/#39-EditMovieCardandCreateCSSExternalFiles

--- src/Ieemdb.Web/ClientApp/src/components/TopRight.js
+++ src/Ieemdb.Web/ClientApp/src/components/TopRight.js
@@ -15,24 +15,26 @@ const responsive = {
 function TopRight(){
     return(
         <Col className="column-right">
-            <div className="title1">
-                <h1 className="title2">{title}</h1>
-            </div>
-            <div>
-                <div style={{width: "100%"}}>
-                    <AliceCarousel
-                        infinite
-                        autoPlay
-                        autoPlayStrategy="none"
-                        autoPlayInterval={3000}
-                        animationDuration={1000}
-                        animationType="fadeout"
-                        mouseTracking
-                        items={items}
-                        responsive={responsive}
-                        disableDotsControls
-                        disableButtonsControls
-                    />
+            <div className="carousel">
+                <div className="title1">
+                    <h1 className="title2">{title}</h1>
+                </div>
+                <div>
+                    <div style={{width: "100%"}}>
+                        <AliceCarousel
+                            infinite
+                            autoPlay
+                            autoPlayStrategy="none"
+                            autoPlayInterval={3000}
+                            animationDuration={1000}
+                            animationType="fadeout"
+                            mouseTracking
+                            items={items}
+                            responsive={responsive}
+                            disableDotsControls
+                            disableButtonsControls
+                        />
+                    </div>
                 </div>
             </div>
         </Col>
