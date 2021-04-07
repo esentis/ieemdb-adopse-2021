5090c0aefbbbf77ef024a886e651cbbe9d3be53c
Teo Mastro <teomastro1999@gmail.com>
2021-04-07 13:28:41 +0300

    removed playbutton and added borders to movieCards

--- src/Ieemdb.Web/ClientApp/src/components/BottomRightCarousel.js
+++ src/Ieemdb.Web/ClientApp/src/components/BottomRightCarousel.js
@@ -4,7 +4,7 @@ import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import MovieCard from './MovieCard';
 
-const items = movies.map(i => <div> {MovieCard(i.id, i.title, i.poster, "200vh", "45%")}</div>);
+const items = movies.map(i => <div> {MovieCard(i.id, i.title, i.poster, "200vh", "45%",false)}</div>);
 const div_style = {display: 'flex', justifyContent: 'center', width: '100%'};
 const responsive = {
     0: { items: 1 },
--- src/Ieemdb.Web/ClientApp/src/components/MovieCard.js
+++ src/Ieemdb.Web/ClientApp/src/components/MovieCard.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './MovieCard.css';
 var svg;
 export default function MovieCard(id, title, poster, height, width, flag){
+    /*
     if (flag) {
         svg = <svg className="btn_play" viewBox="0 0 60 60" onClick={onPlayerClick}><polygon points="0,0 50,30 0,60"/></svg>;
     }
+    */
     function onPosterHover(id) {
         //Mouse on poster visible play button
     }
+    /*
     function onPlayerClick(){
         //Click on play button open youtube frame with trailer
         console.log("Click on player");
     }
+    */
     function onPosterClick(){
         //Click on poster open Movie Page
         console.log("Click on poster");
