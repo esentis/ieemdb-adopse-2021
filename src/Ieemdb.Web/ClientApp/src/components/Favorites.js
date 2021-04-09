68d4680949bef0e3ca2daedcb100c16f669364d1
KostisGrf <34042957+KostisGrf@users.noreply.github.com>
2021-04-09 19:19:34 +0300

    Updates Favorites and Watch List

--- src/Ieemdb.Web/ClientApp/src/components/Favorites.js
+++ src/Ieemdb.Web/ClientApp/src/components/Favorites.js
@@ -1,48 +1,20 @@
 import React from 'react'
-import {Container,Col,Carousel} from 'react-bootstrap';
+import TopRight from './TopRight'
+import MovieCard from './MovieCard';
+import movies from './Movie_Dataset';
 
 function Favorites() {
-    return(
-        <Col className="column-right">
-                <Container className="nav-center">
-                <Carousel>
-                    <Carousel.Item interval={1000}>
-                        <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=First slide&bg=373940"
-                        alt="Favorites"
-                        />
-                        <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={500}>
-                        <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Second slide&bg=282c34"
-                        alt="Second slide"
-                        />
-                        <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=20232a"
-                        alt="Third slide"
-                        />
-                        <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    </Carousel>
-                </Container>
-        </Col>
-    );
+    const items = movies.map(i => <div> {MovieCard(i.id, i.title, i.poster, "250vh", 'auto',true)}</div>);
+    const title='FAVORITES';
+    
+    return (
+        <>
+        <TopRight
+         title={title}
+         items={items}    
+         />
+        </>
+    )
+}
 
-    }
 export default Favorites;
