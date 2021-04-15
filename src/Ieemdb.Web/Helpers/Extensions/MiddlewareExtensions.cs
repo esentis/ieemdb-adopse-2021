186f288fe936ebfc4bfc1985bbda266de8dfb462
George Leonidis <esentakos@yahoo.gr>
2021-04-15 21:03:19 +0300

    🔊 Fine tunes logs for application insights

---
+++ src/Ieemdb.Web/Helpers/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,42 @@
+namespace Esentis.Ieemdb.Web.Helpers.Extensions
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Threading.Tasks;
+
+  using Esentis.Ieemdb.Web.ConfigurationOptions;
+  using Esentis.Ieemdb.Web.Middleware;
+
+  using Microsoft.AspNetCore.Builder;
+  using Microsoft.Extensions.DependencyInjection;
+  using Microsoft.Extensions.Options;
+
+  public static class MiddlewareExtensions
+  {
+    public static IApplicationBuilder UseCorrelation(this IApplicationBuilder app)
+    {
+      if (app == null)
+      {
+        throw new ArgumentNullException(nameof(app));
+      }
+
+      return app.UseMiddleware<CorrelationMIddleware>();
+    }
+
+    public static IServiceCollection AddCorrelation(this IServiceCollection services, CorrelationOptions options)
+    {
+      if (options == null)
+      {
+        throw new ArgumentNullException(nameof(options));
+      }
+
+      services?.AddSingleton(Options.Create(options));
+      return services?.AddSingleton<CorrelationMIddleware>()
+             ?? throw new ArgumentNullException(nameof(services));
+    }
+
+    public static IServiceCollection AddCorrelation(this IServiceCollection services)
+      => services.AddCorrelation(new CorrelationOptions());
+  }
+}
