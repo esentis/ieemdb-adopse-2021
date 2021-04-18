929372fc1c66077dc4998e82faae2fc6fd1b21b1
George Leonidis <esentakos@yahoo.gr>
2021-04-18 18:13:11 +0300

    ✨Scaffolds MovieDto and /api/movie

--- src/Ieemdb.Web.Models/MovieDto.cs
+++ src/Ieemdb.Web.Models/MovieDto.cs
@@ -1,13 +1,42 @@
 namespace Esentis.Ieemdb.Web.Models
 {
+  using System;
+  using System.Collections.Generic;
+
   using Esentis.Ieemdb.Web.Models.Dto;
 
   public class MovieDto
   {
+    public long Id { get; set; }
+
     public string Title { get; set; } = string.Empty;
 
-    public ActorDto Actor { get; set; }
+    public string Plot { get; set; }
 
-    public DirectorDto Director { get; set; }
+    public string TrailerUrl { get; set; }
+
+    public TimeSpan Duration { get; set; }
+
+    public bool Featured { get; set; }
+
+    public double Rating { get; set; }
+
+    public List<GenreDto> Genres { get; set; }
+
+    public DateTimeOffset ReleaseDate { get; set; }
+
+    public string Country { get; set; }
+
+    public List<ActorDto> Actors { get; set; }
+
+    public List<DirectorDto> Directors { get; set; }
+
+    public List<WriterDto> Writers { get; set; }
+
+    public List<ImageDto> Posters { get; set; }
+
+    public List<ImageDto> Banners { get; set; }
+
+    public List<ImageDto> Screenshots { get; set; }
   }
 }
