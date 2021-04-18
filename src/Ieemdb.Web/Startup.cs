47271ef9ee6ba726b8f76a56790afc57b050e9a3
George Leonidis <esentakos@yahoo.gr>
2021-04-18 22:11:21 +0300

    🔧Enables CORS for development purposes

--- src/Ieemdb.Web/Startup.cs
+++ src/Ieemdb.Web/Startup.cs
@@ -164,6 +164,15 @@ namespace Esentis.Ieemdb.Web
       services.AddRazorPages();
 
       services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy("AllowAll",
+          builder =>
+          {
+            builder.AllowAnyOrigin();
+          });
+      });
     }
 
     public void Configure(IApplicationBuilder app)
@@ -196,7 +205,7 @@ namespace Esentis.Ieemdb.Web
       app.UseSpaStaticFiles();
 
       app.UseRouting();
-
+      app.UseCors();
       app.UseReDoc(c =>
       {
         c.RoutePrefix = "docs";
