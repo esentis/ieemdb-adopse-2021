fc84e63dde9067c724a973cfd0609b8eeb118ef3
KostisGrf <34042957+KostisGrf@users.noreply.github.com>
2021-04-22 22:59:43 +0300

    Updates for LoginState

--- src/Ieemdb.Web/ClientApp/src/components/GlobalContext.js
+++ src/Ieemdb.Web/ClientApp/src/components/GlobalContext.js
@@ -2,11 +2,20 @@ import React,{createContext,useContext,useState} from 'react'
 
 const GlobalContext=createContext();
 const UpdateState=createContext();
+const LoginState=createContext();
+const UpdateLoginState=createContext();
 
 export const usePage=()=>{
     return useContext(GlobalContext);
 }
 
+export const useChangeLoginState=()=>{
+    return useContext(UpdateLoginState)
+}
+
+export const useLoginState=()=>{
+    return useContext(LoginState)
+}
 
 export const useUpdatePage=()=>{
     return useContext(UpdateState);
@@ -17,10 +26,20 @@ export const useUpdatePage=()=>{
             setPage(arg);
        }
 
+       const [isLoggedIn,setIsLoggedIn]=useState(false);
+
+       function ChangeLoginState(){
+           setIsLoggedIn(!isLoggedIn);
+       }
+
         return(
             <GlobalContext.Provider value={page}>
             <UpdateState.Provider value={handleClick}>
+            <UpdateLoginState.Provider value={ChangeLoginState}>
+            <LoginState.Provider value={isLoggedIn}>
                 {props.children}
+                </LoginState.Provider>
+                </UpdateLoginState.Provider>
             </UpdateState.Provider>  
             </GlobalContext.Provider>
         )
