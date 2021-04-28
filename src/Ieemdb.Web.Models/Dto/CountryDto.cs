ba920857218ce2d31334d99f53f2a1a06f9dcc99
George Leonidis <esentakos@yahoo.gr>
2021-04-29 00:47:30 +0300

    ♻️ Cleans up code

--- src/Ieemdb.Web.Models/Dto/CountryDto.cs
+++ src/Ieemdb.Web.Models/Dto/CountryDto.cs
@@ -9,6 +9,7 @@ namespace Esentis.Ieemdb.Web.Models.Dto
   public class CountryDto
   {
     public long Id { get; set; }
-    public string CountryOrigin { get; set; }
+
+    public string Name { get; set; }
   }
 }
---
+++ src/Ieemdb.Web.Models/Dto/MovieDto.cs
@@ -0,0 +1,40 @@
+namespace Esentis.Ieemdb.Web.Models
+{
+  using System;
+  using System.Collections.Generic;
+
+  using Esentis.Ieemdb.Web.Models.Dto;
+
+  public class MovieDto
+  {
+    public long Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Plot { get; set; }
+
+    public string TrailerUrl { get; set; }
+
+    public TimeSpan Duration { get; set; }
+
+    public bool Featured { get; set; }
+
+    public double Rating { get; set; }
+
+    public List<GenreDto> Genres { get; set; }
+
+    public DateTimeOffset ReleaseDate { get; set; }
+
+    public List<CountryDto> Countries { get; set; }
+
+    public List<ActorDto> Actors { get; set; }
+
+    public List<DirectorDto> Directors { get; set; }
+
+    public List<WriterDto> Writers { get; set; }
+
+    public List<Uri> Posters { get; set; }
+
+    public List<Uri> Screenshots { get; set; }
+  }
+}
