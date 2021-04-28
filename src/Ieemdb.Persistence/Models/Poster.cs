7b02d3194191cf70f653215e9afb87a12c486c8f
George Leonidis <esentakos@yahoo.gr>
2021-04-29 00:46:22 +0300

    ✨ Adds new models
    Poster and Screenshot

---
+++ src/Ieemdb.Persistence/Models/Poster.cs
@@ -0,0 +1,17 @@
+namespace Esentis.Ieemdb.Persistence.Models
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public class Poster
+  {
+    public int Id { get; set; }
+
+    public Uri Url { get; set; }
+
+    public Movie Movie { get; set; }
+  }
+}
