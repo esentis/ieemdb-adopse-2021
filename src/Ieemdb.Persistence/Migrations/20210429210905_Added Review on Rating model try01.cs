0ca96bcecef89c09d5407c976bdcac7c7fb3fcd5
vagmark <vagmark30@gmail.com>
2021-04-30 00:10:31 +0300

    Added review on rating ++migrations

---
+++ src/Ieemdb.Persistence/Migrations/20210429210905_Added Review on Rating model try01.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Esentis.Ieemdb.Persistence.Migrations
+{
+    public partial class AddedReviewonRatingmodeltry01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Review",
+                table: "Ratings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Review",
+                table: "Ratings");
+        }
+    }
+}
