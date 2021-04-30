29957a20832d246fec5f5d67ee8b66a9ae7e2ff2
Δημήτρης Γαβριηλίδης <49337508+GavriilJim@users.noreply.github.com>
2021-05-01 00:48:53 +0300

    Add fake reviews

--- src/Ieemdb.Web/ClientApp/src/components/MovieViewPoster.js
+++ src/Ieemdb.Web/ClientApp/src/components/MovieViewPoster.js
@@ -129,7 +129,7 @@ function MovieViewPoster(props){
                         <hr className="line"/>
                         <div id="review">
                             <div id="review1">
-                                <p className="revWriter">Petros Apostolopoulos</p>
+                                <p className="revWriter">Fanis Georgiou</p>
                                 <div className="revStars2">
                                     <p className="rating">1/5</p>
                                     <ReactStars {...{value: 1, size: 30, count: 5, color: "black", activeColor: "yellow", isHalf: false, edit: false,
@@ -138,49 +138,36 @@ function MovieViewPoster(props){
                                 </div>
                             </div>
                             <div id="review2">
-                                <p className="revComment">Our comprehensive guidiuyie tooo CSS flexbox layout. This complete guide explains everything about flexbox, focusing on all the different possible properties for the parent element (the flex container) and the child elements (the flex items). It also includes history, demos, patterns, and a browser support chart.</p>
+                                <p className="revComment">I've liked Brie Larson in other films, but she showed ZERO range in this. When your main character in a superhero movie is unwatchable, you already have a problem. In addition, Captain Marvel has no weaknesses, which kills the tension immediately. There is no point at which you feel she is in any danger of losing, or any danger at all for that matter.
+                                                        It's an OK origin story, but it makes no sense as to WHY she's supposedly so powerful. The cat was good.</p>
                             </div>
                         </div>
                         <div id="review">
                             <div id="review1">
                                 <p className="revWriter">Petros Apostolopoulos</p>
                                 <div className="revStars2">
-                                    <p className="rating">1/5</p>
-                                    <ReactStars {...{value: 1, size: 30, count: 5, color: "black", activeColor: "yellow", isHalf: false, edit: false,
+                                    <p className="rating">4/5</p>
+                                    <ReactStars {...{value: 4, size: 30, count: 5, color: "black", activeColor: "yellow", isHalf: false, edit: false,
                                             emptyIcon: <i className="fa fa-star-o" />, halfIcon: <i className="fa fa-star-half" />,
                                             filledIcon: <i className="fa fa-star" />}} />
                                 </div>
                             </div>
                             <div id="review2">
-                                <p className="revComment">Our comprehensive guidiuyie tooo CSS flexbox layout. This complete guide explains everything about flexbox, focusing on all the different possible properties for the parent element (the flex container) and the child elements (the flex items). It also includes history, demos, patterns, and a browser support chart.</p>
+                                <p className="revComment">Over-hyped and very underwhelming and sadly ranks alongside "the Incredible Hulk" in the otherwise incredible MCU. Does not bode well for Avengers: Endgame and Phase 4 if this is the path Marvel is going down</p>
                             </div>
                         </div>
                         <div id="review">
                             <div id="review1">
-                                <p className="revWriter">Petros Apostolopoulos</p>
+                                <p className="revWriter">Giannis Melas</p>
                                 <div className="revStars2">
-                                    <p className="rating">1/5</p>
-                                    <ReactStars {...{value: 1, size: 30, count: 5, color: "black", activeColor: "yellow", isHalf: false, edit: false,
+                                    <p className="rating">3/5</p>
+                                    <ReactStars {...{value: 3, size: 30, count: 5, color: "black", activeColor: "yellow", isHalf: false, edit: false,
                                             emptyIcon: <i className="fa fa-star-o" />, halfIcon: <i className="fa fa-star-half" />,
                                             filledIcon: <i className="fa fa-star" />}} />
                                 </div>
                             </div>
                             <div id="review2">
-                                <p className="revComment">Our comprehensive guidiuyie tooo CSS flexbox layout. This complete guide explains everything about flexbox, focusing on all the different possible properties for the parent element (the flex container) and the child elements (the flex items). It also includes history, demos, patterns, and a browser support chart.</p>
-                            </div>
-                        </div>
-                        <div id="review">
-                            <div id="review1">
-                                <p className="revWriter">Petros Apostolopoulos</p>
-                                <div className="revStars2">
-                                    <p className="rating">1/5</p>
-                                    <ReactStars {...{value: 1, size: 30, count: 5, color: "black", activeColor: "yellow", isHalf: false, edit: false,
-                                            emptyIcon: <i className="fa fa-star-o" />, halfIcon: <i className="fa fa-star-half" />,
-                                            filledIcon: <i className="fa fa-star" />}} />
-                                </div>
-                            </div>
-                            <div id="review2">
-                                <p className="revComment">Our comprehensive guidiuyie tooo CSS flexbox layout. This complete guide explains everything about flexbox, focusing on all the different possible properties for the parent element (the flex container) and the child elements (the flex items). It also includes history, demos, patterns, and a browser support chart.</p>
+                                <p className="revComment">Nothing new is brought to the table here. For a superhero/origin story, a fish-out-of-water, espionage, 90's nostalgia-hit, a spectaculay disinteresting film. None of the actors seem keen on being there, in particular the star. Anyone who's seen 'Room' is more than aware of Brie Larson's acting chops, and here they are on full disguise. Her displayed emotions throughout the film range from smugness to total emotional emptiness. Boring story, predictable and unsatisfying twist, low-quality humour (if any) and someone please send Ben Mendelsohn to a speech pathologist to fix the lisp, and then to a linguistics specialist to remind him and the writers that just because the MCU has given the universe English for a common language, doesn't mean they'll use the same phrases as us. Not worth a watch unless you're a stickler for all MCU content, as I unfortunately am.</p>
                             </div>
                         </div>
                         <div id="add_review">
