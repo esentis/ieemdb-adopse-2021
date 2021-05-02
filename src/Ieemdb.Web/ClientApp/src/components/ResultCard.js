c930043a52b395299a9ab239678b704abe1cae3a
KostisGrf <34042957+KostisGrf@users.noreply.github.com>
2021-05-02 03:20:21 +0300

    Updates  cards

--- src/Ieemdb.Web/ClientApp/src/components/ResultCard.js
+++ src/Ieemdb.Web/ClientApp/src/components/ResultCard.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Moment from "react-moment";
 import '../Styles/ResultCard.css'
 import {useHistory} from 'react-router-dom';
 
-const ResultCard = (props) => {
 
+const ResultCard = (props) => {
+  var results=[];
   const history=useHistory();
+  const [disable, setDisable] = useState(false)
 
   function MovieClick(){
     history.push("/Movie/"+props.id);
   }
-
+  if(props.flag){
+     results=props.featured.map(i=>i.id);
+  }
+  
+  useEffect(()=>{
+    if(results.find(x=>x===props.id)){
+      setDisable(true);}
+     else{setDisable(false)}
+  },[results,props.id])
+  
+  
   return (    
     <div className="result-card">
       <div className="poster-wrapper">
@@ -27,6 +39,17 @@ const ResultCard = (props) => {
             <Moment format="YYYY">{props.release_date}</Moment>
           </h4>
         </div>
+        
+          
+          {props.flag?<button
+            className="addToFeaturedButton"
+            disabled={disable}
+            onClick={() => props.onClick(props.id,props.poster,props.title)}
+          >
+            Add to Featured
+          </button>:""}
+          
+          
     </div>
     </div>
   );
