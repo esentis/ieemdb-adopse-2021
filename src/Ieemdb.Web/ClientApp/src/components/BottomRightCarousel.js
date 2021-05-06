bd51f798ca825e3ccd32d4fed4677d982ed63ea1
Teo Mastro <teomastro1999@gmail.com>
2021-05-06 11:43:36 +0300

    WIP carousels

--- src/Ieemdb.Web/ClientApp/src/components/BottomRightCarousel.js
+++ src/Ieemdb.Web/ClientApp/src/components/BottomRightCarousel.js
@@ -3,13 +3,31 @@ import movies from './Movie_Dataset';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import MovieCard from './MovieCard';
+import { Nav, Col } from 'react-bootstrap';
 import '../Styles/BottomRightCarousel.css'
 const responsive = {
     0: { items: 2 },
     568: { items: 3 },
     1024: { items: 6 },
 };
-function BottomRightCarousel(){
+function BottomRightCarousel() {
+
+    function showNewReleases() {
+      
+    }
+
+    function showPopular() {
+      console.log('bbbbb')
+    }
+
+    function showRecentlyAdded() {
+      console.log('ccccc')
+    }
+
+    function showTopRated() {
+      console.log('ddddd')
+    }
+
     const items = movies.map(i => <MovieCard
         id={i.id}
         Title={i.title} 
@@ -30,7 +48,34 @@ function BottomRightCarousel(){
     const slidePrev = () => setActiveIndex(activeIndex - 1);
     const slideNext = () => setActiveIndex(activeIndex + 1);
     const syncActiveIndex = ({ item }) => setActiveIndex(item);
-    return(
+    return (
+        <div>
+        <Nav className="nav-bottom-right">
+            <Col>
+            </Col>
+            <Col>
+            <Nav.Item>
+                <Nav.Link className="nav-bottom-right-link" onClick={showNewReleases}>New Releases</Nav.Link>
+                </Nav.Item>
+            </Col>
+            <Col>
+                <Nav.Item>
+                    <Nav.Link className="nav-bottom-right-link" onClick={showPopular}>Popular</Nav.Link>
+                </Nav.Item>
+            </Col>
+            <Col>
+                <Nav.Item>
+                    <Nav.Link className="nav-bottom-right-link" onClick={showRecentlyAdded}>Recently Added</Nav.Link>
+                </Nav.Item>
+            </Col>
+            <Col>
+                <Nav.Item>
+                    <Nav.Link className="nav-bottom-right-link" onClick={showTopRated}>Top Rated</Nav.Link>
+                </Nav.Item>
+            </Col>
+            <Col>
+            </Col>
+        </Nav>
         <div className="div_style">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <button className="button_arrow" onClick={slidePrev}><i id="arrow" className="fa fa-arrow-left"></i></button>
@@ -49,7 +94,8 @@ function BottomRightCarousel(){
                 />
             </div>
             <button className="button_arrow" onClick={slideNext}><i id="arrow" className="fa fa-arrow-right"></i></button>
-        </div>   
+        </div>
+        </div>
     );
 }
 export default BottomRightCarousel;
