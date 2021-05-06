4456eedd86f358c03a8c4e1b3947c5e39b12e5b8
Teo Mastro <teomastro1999@gmail.com>
2021-05-06 14:43:56 +0300

    WIP more more

--- src/Ieemdb.Web/ClientApp/src/components/BottomRightCarousel.js
+++ src/Ieemdb.Web/ClientApp/src/components/BottomRightCarousel.js
@@ -11,39 +11,72 @@ const responsive = {
     1024: { items: 6 },
 };
 function BottomRightCarousel() {
-
-    function showNewReleases() {
-      
+    const [choose, setChoose] = useState("");
+    
+    function showNewReleases(number) {
+      setChoose(number);
+      console.log(choose);
     }
 
-    function showPopular() {
-      console.log('bbbbb')
+    function showPopular(number) {
+      setChoose(number);
     }
 
-    function showRecentlyAdded() {
-      console.log('ccccc')
+    function showRecentlyAdded(number) {
+      setChoose(number);
     }
 
-    function showTopRated() {
-      console.log('ddddd')
+    function showTopRated(number) {
+      setChoose(number);
     }
 
-    const items = movies.map(i => <MovieCard
-        id={i.id}
-        Title={i.title} 
-        Poster={i.poster} 
-        Overview={i.overview}
-        ReleaseDate={i.release_date}
-        Genres={i.genres}
-        Actors={i.actors}
-        Writers={i.writers}
-        Directors={i.directors}
-        Rating={i.rating}
-        Duration={i.duration}
-        CountryOrigin={i.countryOrigin}
-        height={"200vh"} 
-        width={"140vw"} />
-        );
+    function DecideCarousel() {
+        const [activeIndex, setActiveIndex] = useState(0);
+        const syncActiveIndex = ({ item }) => setActiveIndex(item);
+        if(choose === "1") {
+
+        }
+        else if (choose === "2") {
+
+        }
+        else if (choose === "3") {
+
+        }
+        else if (choose === "4") {
+
+        }
+        else { //to default me to poy fortwnei h selida
+          const items = movies.map(i => <MovieCard
+            id={i.id}
+            Title={i.title}
+            Poster={i.poster}
+            Overview={i.overview}
+            ReleaseDate={i.release_date}
+            Genres={i.genres}
+            Actors={i.actors}
+            Writers={i.writers}
+            Directors={i.directors}
+            Rating={i.rating}
+            Duration={i.duration}
+            CountryOrigin={i.countryOrigin}
+            height={"200vh"}
+            width={"140vw"} />
+          );
+          return <AliceCarousel
+            activeIndex={activeIndex}
+            infinite
+            autoPlayStrategy="none"
+            animationType="fadeout"
+            mouseTracking
+            items={items}
+            responsive={responsive}
+            disableDotsControls
+            disableButtonsControls
+            onSlideChanged={syncActiveIndex}
+          />;
+        }
+      }
+    
     const [activeIndex, setActiveIndex] = useState(0);
     const slidePrev = () => setActiveIndex(activeIndex - 1);
     const slideNext = () => setActiveIndex(activeIndex + 1);
@@ -54,23 +87,23 @@ function BottomRightCarousel() {
             <Col>
             </Col>
             <Col>
-            <Nav.Item>
-                <Nav.Link className="nav-bottom-right-link" onClick={showNewReleases}>New Releases</Nav.Link>
+                <Nav.Item>
+                  <Nav.Link className="nav-bottom-right-link" onClick={() => showNewReleases("1")}>New Releases</Nav.Link>
                 </Nav.Item>
             </Col>
             <Col>
                 <Nav.Item>
-                    <Nav.Link className="nav-bottom-right-link" onClick={showPopular}>Popular</Nav.Link>
+                  <Nav.Link className="nav-bottom-right-link" onClick={() => showPopular("2")}>Popular</Nav.Link>
                 </Nav.Item>
             </Col>
             <Col>
                 <Nav.Item>
-                    <Nav.Link className="nav-bottom-right-link" onClick={showRecentlyAdded}>Recently Added</Nav.Link>
+                  <Nav.Link className="nav-bottom-right-link" onClick={() => showRecentlyAdded("3")}>Recently Added</Nav.Link>
                 </Nav.Item>
             </Col>
             <Col>
                 <Nav.Item>
-                    <Nav.Link className="nav-bottom-right-link" onClick={showTopRated}>Top Rated</Nav.Link>
+                  <Nav.Link className="nav-bottom-right-link" onClick={() => showTopRated("4")}>Top Rated</Nav.Link>
                 </Nav.Item>
             </Col>
             <Col>
@@ -80,18 +113,7 @@ function BottomRightCarousel() {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <button className="button_arrow" onClick={slidePrev}><i id="arrow" className="fa fa-arrow-left"></i></button>
             <div className="carousel_style">
-                <AliceCarousel
-                    activeIndex={activeIndex}
-                    infinite
-                    autoPlayStrategy="none"
-                    animationType="fadeout"
-                    mouseTracking
-                    items={items}
-                    responsive={responsive}
-                    disableDotsControls
-                    disableButtonsControls
-                    onSlideChanged={syncActiveIndex}
-                />
+              <DecideCarousel />
             </div>
             <button className="button_arrow" onClick={slideNext}><i id="arrow" className="fa fa-arrow-right"></i></button>
         </div>
