1353b91e2ac853c853afe91a6091e1aac64ffb42
KostisGrf <34042957+KostisGrf@users.noreply.github.com>
2021-05-07 00:42:47 +0300

    Adds snackBar on error

--- src/Ieemdb.Web/ClientApp/src/components/LoginForm.js
+++ src/Ieemdb.Web/ClientApp/src/components/LoginForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../Styles/Forms.css';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 /*import auth from './auth';*/
 import { useHistory } from 'react-router-dom';
 import {/*useLoginState,*/ useChangeLoginState } from './GlobalContext';
@@ -9,8 +11,14 @@ function LoginForm() {
   const [passwordLogin, setPasswordLogin] = useState("")
   /*const [deviceName, setDeviceName] = useState("");*/
   const history = useHistory();
-  const isLoggedIn = useChangeLoginState();
-  const handleSubmitLogin = async (evt) => {
+  const [open,setOpen]=useState(false);
+        function Alert(props) {
+            return <MuiAlert elevation={6} variant="filled" {...props} />;
+          }
+        
+    function CloseSnackBar(){setOpen(false)};    
+    const isLoggedIn = useChangeLoginState();
+    const handleSubmitLogin = async (evt) => {
     evt.preventDefault();
     const url = 'https://' + window.location.host + '/api/account/login';
     axios.post(url, {
@@ -18,14 +26,17 @@ function LoginForm() {
       password: passwordLogin,
       deviceName: 'fdsfadsfas' //na perasw deviceName
     }).then(function (res) {
-      // console.log(res);
       //console.log(res.data.accessToken);
       //console.log(res.status);
       if (res.status === 200) {
         localStorage.setItem('token', res.data.accessToken);
+        localStorage.setItem('username',userNameLogin);
         isLoggedIn(true);
         history.push("/");
       }
+    })
+    .catch(error=>{
+      setOpen(true);
     });
   }
   return (
@@ -38,6 +49,11 @@ function LoginForm() {
         <input className="formInput" type="password" placeholder="Your password here" value={passwordLogin} onChange={e => setPasswordLogin(e.target.value)}/>
         <input className="formButton" type="submit" value="Login" />
       </form>
+      <div className="snackbar"><Snackbar open={open} autoHideDuration={6000} onClose={CloseSnackBar} anchorOrigin={{vertical:'bottom',horizontal:'center'}}  >
+            <Alert onClose={CloseSnackBar} severity={"error"}>
+            Wrong username/password
+        </Alert>
+      </Snackbar>  </div>
     </div>
   );
 }
