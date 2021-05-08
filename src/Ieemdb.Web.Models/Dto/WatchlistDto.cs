bdaeebeb3ff28ae1a19998049d5a0a0d018b330a
vagmark <vagmark30@gmail.com>
2021-05-09 02:20:48 +0300

    Added watchlist dto

---
+++ src/Ieemdb.Web.Models/Dto/WatchlistDto.cs
@@ -0,0 +1,16 @@
+namespace Esentis.Ieemdb.Web.Models.Dto
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public class WatchlistDto
+  {
+    public long WatchlistId { get; set; }
+
+    public string Name { get; set; }
+
+  }
+}
