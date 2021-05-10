092c2ff13f7bd40739fe05911b0f35ce709585c1
Δημήτρης Γαβριηλίδης <49337508+GavriilJim@users.noreply.github.com>
2021-05-11 01:58:18 +0300

    Movie duration from poster to synopsis

--- src/Ieemdb.Web/ClientApp/src/components/MovieView.js
+++ src/Ieemdb.Web/ClientApp/src/components/MovieView.js
@@ -17,9 +17,9 @@ function MovieView() {
     })
     return (
         <Col className='column-right-MovieView'>
-            <div className='MovieViewPoster'><MovieViewPoster key={item.id} id={item.id} title={item.title} poster={item.poster} releaseDate={item.release_date} genres={item.genres} rating={item.rating} duration={item.duration}/></div>
+            <div className='MovieViewPoster'><MovieViewPoster key={item.id} id={item.id} title={item.title} poster={item.poster} releaseDate={item.release_date} genres={item.genres} rating={item.rating}/></div>
             <div className='splitScreen'>
-                <div className='MovieViewSynopsis'><MovieViewSynopsis key={item.id} id={item.id} overview={item.overview} actors={item.actors} writers={item.writers} directors={item.directors} countryOrigin={item.countryOrigin}/></div>
+                <div className='MovieViewSynopsis'><MovieViewSynopsis key={item.id} id={item.id} overview={item.overview} actors={item.actors} writers={item.writers} directors={item.directors} countryOrigin={item.countryOrigin} duration={item.duration}/></div>
                 <div className='MovieViewTrailer'><MovieViewTrailer id={item.id}/></div>
             </div>
         </Col>
