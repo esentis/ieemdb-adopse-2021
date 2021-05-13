8e57d7a7ed8fb1a6d5d5d4eef3245b1bb7ba791e
George Leonidis <esentakos@yahoo.gr>
2021-05-13 23:39:43 +0300

    🐛 Removes serialization fields

--- src/Ieemdb.Web/Providers/Models/DetailedMovie.cs
+++ src/Ieemdb.Web/Providers/Models/DetailedMovie.cs
@@ -2,29 +2,16 @@ namespace Esentis.Ieemdb.Web.Providers.Models
 {
   public class DetailedMovie
   {
-    public bool adult { get; set; }
-    public string backdrop_path { get; set; }
-    public object belongs_to_collection { get; set; }
-    public int budget { get; set; }
     public Genre[] genres { get; set; }
-    public string homepage { get; set; }
     public int id { get; set; }
-    public string imdb_id { get; set; }
-    public string original_language { get; set; }
-    public string original_title { get; set; }
     public string overview { get; set; }
-    public float popularity { get; set; }
     public object poster_path { get; set; }
     public Production_Countries[] production_countries { get; set; }
     public string release_date { get; set; }
-    public int revenue { get; set; }
     public int runtime { get; set; }
     public string status { get; set; }
-    public string tagline { get; set; }
     public string title { get; set; }
     public bool video { get; set; }
-    public float vote_average { get; set; }
-    public int vote_count { get; set; }
   }
 
   public class Production_Countries
--- src/Ieemdb.Web/Providers/Models/MovieDB.cs
+++ src/Ieemdb.Web/Providers/Models/MovieDB.cs
@@ -13,15 +13,9 @@ namespace Esentis.Ieemdb.Web.Models.MovieDBDto
 
   public class MovieDB
   {
-    public object poster_path { get; set; }
     public string overview { get; set; }
-    public string release_date { get; set; }
-    public int[] genre_ids { get; set; }
     public long id { get; set; }
-    public string original_title { get; set; }
     public string title { get; set; }
-    public object backdrop_path { get; set; }
-    public float popularity { get; set; }
-    public bool video { get; set; }
+
   }
 }
